f5ebde449980cf4340c6f3e9d6024fd2c70f485b
Oowazu Nonowazu <oowazu.nonowazu@gmail.com>
2020-05-31 23:46:12 -0500

    More updates to director update events

--- nari/parser/event/directorupdate.py
+++ nari/parser/event/directorupdate.py
@@ -1,6 +1,6 @@
 from nari.parser.event.base import Event
 from nari.types.event import Type
-from nari.types.director import DirectorUpdateType
+from nari.types.director import DirectorUpdateType, DirectorUpdateCommand
 
 class DirectorUpdate(Event):
     """Can apparently do several things:
@@ -13,9 +13,10 @@ class DirectorUpdate(Event):
     def handle_params(self):
         self.director_type = int(self.params[0][:4], 16)
         self.instance_id = int(self.params[0][4:], 16)
-        # still not sure about these
-        # param[1] - Command? Cactbot notes that 40000010 is a wipe, for example
-        # param[2:6] - 4 bytes of arbitrary data 
+        self.director_command = int(self.params[1], 16)
+        # param[2:6] - 4 bytes of arbitrary data, dependant on the director command
     
     def __repr__(self):
-        return f'<DirectorUpdate ({DirectorUpdateType.name_for_value(self.director_type)})>'
+        dtype = DirectorUpdateType.name_for_value(self.director_type)
+        dcomm = DirectorUpdateCommand.name_for_value(self.director_command)
+        return f'<DirectorUpdate ({dtype}|{dcomm})>'
