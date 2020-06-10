eb4814cd4e9960d54739f1165266edb38a7c7093
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2020-06-10 13:33:35 +1000

    Update NetworkTargetHeadMarker docs for known param details

--- nari/types/event/networktargetheadmarker.py
+++ nari/types/event/networktargetheadmarker.py
@@ -5,17 +5,17 @@ from nari.types.actor import Actor
 from nari.types.event import Type
 
 class NetworkTargetHeadMarker(Event):
-    """Looks like it identifies the type of marker over someone's head iunno"""
+    """Sets the model to use for the marker over a player's head"""
     __id__ = Type.networktargetheadmarker.value
     def handle_params(self):
         self.target_actor = Actor(self.params[0], self.params[1])
         # there's 6 other bytes afterwards:
-        # params[2]: dunno
-        # params[3]: dunno
-        # params[4]: I think this is the type of marker
-        # params[5]: dunno
-        # params[6]: dunno
-        # params[7]: dunno
+        # params[2]: junk, literally padding from the Message IPC header
+        # params[3]: padding
+        # params[4]: model ID
+        # params[5]: unused
+        # params[6]: unused
+        # params[7]: unused
 
     def __repr__(self):
         return f'<HeadMarker ({self.target_actor})>'
