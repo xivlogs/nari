04c0e3317969707f4ba6dfc48d896ddb46372719
Oowazu Nonowazu <oowazu.nonowazu@gmail.com>
2020-06-24 14:28:36 -0500

    Add batch writes to writer

--- nari/io/writer/__init__.py
+++ nari/io/writer/__init__.py
@@ -11,15 +11,30 @@ class Writer(metaclass=ABCMeta):
     def __init__(self, stream: Iterator[Event]):
         self.stream = iter(stream)
 
+    def write_one(self) -> bool:
+        """Attempts to write one event. If it fails, return False. Otherwise, return True"""
+        try:
+            new_event = next(self.stream)
+            self.write_next(new_event)
+        except StopIteration:
+            return False
+
+        return True
+
+    def write_batch(self, amount: int):
+        """Writes a batch of events"""
+        for i in range(amount): # pylint: disable=unused-variable
+            response = self.write_one()
+            if not response:
+                # best to raise an exception, but I dunno
+                return
+
     def write(self):
-        """Prompts the writer to begin consuming events"""
+        """Prompts the writer to consume all events"""
         while True:
-            # try to grab a single event
-            try:
-                new_event = next(self.stream)
-                self.write_next(new_event)
-            except StopIteration:
-                break
+            response = self.write_one()
+            if not response:
+                return
 
     @abstractmethod
     def write_next(self, event: Event) -> None:
