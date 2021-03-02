e9383bb8e5d1bafb00f41045ac2253f0c764bf35
Joseph Salmingo <joseph.salmingo@gmail.com>
2021-03-02 10:14:57 +0900

    first implementation of gauge parser

--- nari/util/exceptions.py
+++ nari/util/exceptions.py
@@ -5,3 +5,6 @@ class EventNotFound(Exception):
 
 class InvalidChecksum(Exception):
     """Thrown when the checksum of an event doesn't match"""
+
+class JobGaugeNotFound(Exception):
+    """Thrown when gauge data is not found for job"""
