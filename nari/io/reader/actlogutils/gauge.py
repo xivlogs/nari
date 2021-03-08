1a0f0fd879cdbfc8caabb11f3108991d673d62ca
jooooooooe <joseph.salmingo@gmail.com>
2021-03-08 16:49:12 +0900

    better description of gauge data and fixing param slice

--- nari/io/reader/actlogutils/gauge.py
+++ nari/io/reader/actlogutils/gauge.py
@@ -8,11 +8,11 @@ def gauge_from_logline(timestamp: datetime, params: List[str]) -> Gauge:
     """Parses gauge event out from act log line"""
     # Param layout from ACT
     # 0 - 'actor id' which is the player who is logging.
-    # 1-4 - Most gauge data is stored in 1-3. Gauge data and data type is dependent on job so they are left as bytes for
-    #       destructuring later. I have no idea what's in 4 and could not find documentation for it.
+    # 1-4 - Gauge data is from a C-style union type stored with fields depending on job. ACT represents this as four
+    #       32-bit unsigned ints so they're left as bytes for destructuring later.
 
     actor_id = int(params[0], 16)
-    gauge_data = tuple((string_to_bytes(param) for param in params[1:5]))
+    gauge_data = tuple((string_to_bytes(param) for param in params[1:]))
 
     # TODO: add exceptions for IndexError
 
@@ -23,7 +23,7 @@ def string_to_bytes(param: str) -> bytes:
     """Helper function to convert strings into bytes. Also changes the byte order."""
 
     param_expanded = param.zfill(8)
-    bytes_data = int(param_expanded, 16).to_bytes(4, "little")
+    bytes_arr = bytearray.fromhex(param_expanded)
+    bytes_arr.reverse()  # switch the endianness
 
-    # TODO: add exceptions for ValueError and Overflow Error
-    return bytes_data
+    return bytes(bytes_arr)
