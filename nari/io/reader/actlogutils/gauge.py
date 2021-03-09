e3ec7638c01967b91cdc9e5e9af83c6f4a6729b1
jooooooooe <joseph.salmingo@gmail.com>
2021-03-09 18:17:52 +0900

    renamed byte function, moved other byte function

--- nari/io/reader/actlogutils/gauge.py
+++ nari/io/reader/actlogutils/gauge.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from typing import List
 
 from nari.types.event.gauge import Gauge
+from nari.util.byte import hexstr_to_bytes
 
 
 def gauge_from_logline(timestamp: datetime, params: List[str]) -> Gauge:
@@ -12,18 +13,8 @@ def gauge_from_logline(timestamp: datetime, params: List[str]) -> Gauge:
     #       32-bit unsigned ints so they're left as bytes for destructuring later.
 
     actor_id = int(params[0], 16)
-    gauge_data = tuple((string_to_bytes(param) for param in params[1:]))
+    gauge_data = tuple((hexstr_to_bytes(param) for param in params[1:]))
 
     # TODO: add exceptions for IndexError
 
     return Gauge(timestamp=timestamp, actor_id=actor_id, fields=gauge_data)
-
-
-def string_to_bytes(param: str) -> bytes:
-    """Helper function to convert strings into bytes. Also changes the byte order."""
-
-    param_expanded = param.zfill(8)
-    bytes_arr = bytearray.fromhex(param_expanded)
-    bytes_arr.reverse()  # switch the endianness
-
-    return bytes(bytes_arr)
