d29b19e85dae37fa66dff961d519a9d86d2b4c63
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2022-04-09 13:51:21 +1000

    Update params for visibility events

--- nari/types/event/visibility.py
+++ nari/types/event/visibility.py
@@ -6,7 +6,7 @@ from nari.types.event import Event
 from nari.types.actor import Actor
 
 # pylint: disable=invalid-name
-class VisibilityType(Enum):
+class VisibilityCategory(Enum):
     """Enums for concepts that have visibility"""
     Nameplate = auto()
 
@@ -22,13 +22,13 @@ class VisibilityChange(Event): # pylint: disable=too-few-public-methods
     def __init__(self, *,
                  timestamp: Timestamp,
                  actor: Actor,
-                 visibility_type: VisibilityType,
+                 visibility_category: VisibilityCategory,
                  state: VisibilityState,
                 ):
         super().__init__(timestamp)
         self.actor = actor
-        self.visibility_type = visibility_type
+        self.visibility_category = visibility_category
         self.state = state
 
     def __repr__(self):
-        return f'<Visibility {self.visibility_type.name}>'
+        return f'<Visibility {self.visibility_category.name}>'
