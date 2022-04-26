36175386e4575d333d2b2d45c7a4c39d4ecbee7d
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2022-04-26 11:50:35 +1000

    Fix deprecation warning

--- test/actlog/test_act_utils.py
+++ test/actlog/test_act_utils.py
@@ -10,7 +10,7 @@ class TestLineChecksum(unittest.TestCase):
         Tests act checksum validation for lines
         """
         test_line = "253|2022-02-09T20:09:52.6303877-06:00|FFXIV_ACT_Plugin Version: 2.6.4.1 (50BCD605C50A749F)|5401dc333f466389"
-        self.assertEquals(validate_checksum(test_line, 1), True)
+        self.assertEqual(validate_checksum(test_line, 1), True)
 
 
 if __name__ == '__main__':
