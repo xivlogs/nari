353272f77f86762ca5c7dca2ce9d5349d86e48e9
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2022-04-26 15:52:46 +1000

    Update test class names for what they're actually doing

--- test/actlog/test_updatehpmp_parsing.py
+++ test/actlog/test_updatehpmp_parsing.py
@@ -4,7 +4,7 @@ from nari.io.reader.actlogutils.updatehpmp import updatehpmp_from_logline
 from nari.types.event.updatehpmp import UpdateHpMp
 
 
-class TestLineChecksum(unittest.TestCase):
+class TestUpdateHPMPFromACTLogLine(unittest.TestCase):
     def test_line_when_sp_is_empty(self):
         timestamp = 0000
         show_default_hpmp = '106652C6||50875||10000|||||||'.split('|')
