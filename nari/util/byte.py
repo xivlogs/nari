e43d72e03c26018f59fa03209fd6e9db245dd72b
Oowazu Nonowazu <oowazu.nonowazu@gmail.com>
2022-07-18 12:04:42 -0500

    Add option to flip bytes

--- nari/util/byte.py
+++ nari/util/byte.py
@@ -8,11 +8,12 @@ def hexstr_to_int(string: str, pad: bool = True) -> int:
     return int(string, 16)
 
 
-def hexstr_to_bytes(param: str) -> bytes:
+def hexstr_to_bytes(param: str, reverse: bool = False) -> bytes:
     """Helper function to convert strings into bytes. Also changes the byte order."""
-
     param_expanded = param.zfill(8)
     bytes_arr = bytearray.fromhex(param_expanded)
-    bytes_arr.reverse()  # switch the endianness
+
+    if reverse:
+        bytes_arr.reverse()  # switch the endianness
 
     return bytes(bytes_arr)
