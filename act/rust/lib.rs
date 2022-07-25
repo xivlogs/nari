debb6bd61976c037c5f961875107d57d5e6c2212
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2022-07-25 14:05:09 +1000

    Hack around python imports

--- act/rust/lib.rs
+++ act/rust/lib.rs
@@ -39,5 +39,10 @@ fn nari_act_ext(py: Python, m: &PyModule) -> PyResult<()> {
     utils_module.add_function(wrap_pyfunction!(utils::pad8, m)?)?;
     m.add_submodule(utils_module)?;
 
+    // HACK: abuse python imports to make `from rustext.utils import validate_checksum` work
+    let sys: &PyModule = py.import("sys").unwrap();
+    sys.getattr("modules")?.set_item("nari_act_ext.parser", parser_module)?;
+    sys.getattr("modules")?.set_item("nari_act_ext.utils", utils_module)?;
+
     Ok(())
 }
