c158cd4519637ff5559cfcd67ac7b19482da6b95
wolfcomp <magnus1997.mho@gmail.com>
2022-10-05 21:30:17 +0200

    I'm to used to C# or C++ if checks

--- nari/util/pair.py
+++ nari/util/pair.py
@@ -1,13 +1,13 @@
 """Defines a helper class to bundle string hex IDs with an associated name"""
 
-from ..types import HexStr
 from typing import Union
+from ..types import HexStr
 
 # pylint: disable=too-few-public-methods,redefined-builtin
 class IdNamePair():
     """Represents a tuple where there's a hex or an int id and a string name; automatically converts the hex string to an int"""
     def __init__(self, id: Union[HexStr, int], name: str):
-        if(isinstance(id, int)):
+        if isinstance(id, int):
             self.id = id
         else:
             self.id = int(id, 16)
