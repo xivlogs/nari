336da0993491df1830074b63732b9bd215652c4b
wolfcomp <magnus1997.mho@gmail.com>
2022-10-05 21:54:32 +0200

    use python 3.10 implied union

--- nari/util/pair.py
+++ nari/util/pair.py
@@ -1,12 +1,11 @@
 """Defines a helper class to bundle string hex IDs with an associated name"""
 
-from typing import Union
 from ..types import HexStr
 
 # pylint: disable=too-few-public-methods,redefined-builtin
 class IdNamePair():
     """Represents a tuple where there's a hex or an int id and a string name; automatically converts the hex string to an int"""
-    def __init__(self, id: Union[HexStr, int], name: str):
+    def __init__(self, id: HexStr | int, name: str):
         if isinstance(id, int):
             self.id = id
         else:
