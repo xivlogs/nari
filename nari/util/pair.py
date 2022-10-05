8996906d25876e294f49b845a8ab2ded16b68fda
wolfcomp <magnus1997.mho@gmail.com>
2022-10-05 21:26:23 +0200

    allow for integers to be passed to IdNamePair

--- nari/util/pair.py
+++ nari/util/pair.py
@@ -1,12 +1,16 @@
 """Defines a helper class to bundle string hex IDs with an associated name"""
 
 from ..types import HexStr
+from typing import Union
 
 # pylint: disable=too-few-public-methods,redefined-builtin
 class IdNamePair():
-    """Represents a tuple where there's a hex id and a string name; automatically converts the hex string to an int"""
-    def __init__(self, id: HexStr, name: str):
-        self.id = int(id, 16)
+    """Represents a tuple where there's a hex or an int id and a string name; automatically converts the hex string to an int"""
+    def __init__(self, id: Union[HexStr, int], name: str):
+        if(isinstance(id, int)):
+            self.id = id
+        else:
+            self.id = int(id, 16)
         self.name = name
 
     def __repr__(self):
