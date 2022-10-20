8dc8f83211ca34b2f4dfb010aae4a6abb0f06937
Oowazu Nonowazu <oowazu.nonowazu@gmail.com>
2022-10-20 18:41:37 -0500

    add marker docs

--- nari/types/event/markers.py
+++ nari/types/event/markers.py
@@ -1,4 +1,7 @@
-"""Classes for overhead markers"""
+"""
+Markers and combat-related vfx
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
 from enum import IntEnum
 
 from nari.types import Timestamp
@@ -7,7 +10,15 @@ from nari.types.actor import Actor
 
 
 class OverheadVFX(Event): # pylint: disable=too-few-public-methods
-    """Event representing content-applied markers"""
+    """Event representing content-applied markers
+
+    :param timestamp: The timestamp of the event
+    :type timestamp: Timestamp
+    :param actor: The actor the VFX is being applied to
+    :type actor: Actor
+    :param marker_id: The id of the vfx
+    :type marker_id: int
+    """
     def __init__(self, *,
                  timestamp: Timestamp,
                  actor: Actor,
@@ -25,28 +36,46 @@ class OverheadVFX(Event): # pylint: disable=too-few-public-methods
 class MarkerOperation(IntEnum):
     """Enums for marker operations"""
     Unknown = 0
+    """The operation is unknown"""
     Add = 1
+    """The marker is added"""
     Update = 2
+    """The marker is updated"""
     Delete = 3
+    """The marker is removed"""
 
 
 # pylint: disable=invalid-name
 class PlayerMarker(IntEnum):
     """Enums for player-applied markers, these IDs can be found in the Marker.exd client file"""
     Attack1 = 0
+    """Places the (1) marker over the target"""
     Attack2 = 1
+    """Places the (2) marker over the target"""
     Attack3 = 2
+    """Places the (3) marker over the target"""
     Attack4 = 3
+    """Places the (4) marker over the target"""
     Attack5 = 4
+    """Places the (5) marker over the target"""
     Bind1 = 5
+    """Places the chain marker (with a 1) over the target"""
     Bind2 = 6
+    """Places the chain marker (with a 2) over the target"""
     Bind3 = 7
+    """Places the chain marker (with a 3) over the target"""
     Ignore1 = 8
+    """Places the ignore marker (with a 1) over the target"""
     Ignore2 = 9
+    """Places the ignore marker (with a 2) over the target"""
     Square = 10
+    """Places a square marker over the target"""
     Circle = 11
+    """Places a circle marker over the target"""
     Plus = 12
+    """Places a plus marker over the target"""
     Triangle = 13
+    """Places a triangle marker over the target"""
 
     @classmethod
     def contains(cls, value: int) -> bool:
@@ -55,7 +84,19 @@ class PlayerMarker(IntEnum):
 
 
 class OverheadMarker(Event): # pylint: disable=too-few-public-methods
-    """Event representing player-applied markers"""
+    """Event representing player-applied markers
+
+    :param timestamp: The timestamp of the event
+    :type timestamp: Timestamp
+    :param source_actor: The actor that applied the marker
+    :type source_actor: Actor
+    :param target_actor: The target that receives the marker
+    :type target_actor: Actor
+    :param operator: If the marker is being added/removed/modified
+    :type operator: MarkerOperation
+    :param marker: The marker that goes over the head
+    :type marker: PlayerMarker
+    """
     def __init__(self, *,
                  timestamp: Timestamp,
                  source_actor: Actor,
