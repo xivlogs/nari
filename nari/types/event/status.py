e04c7505b4a9000e9fbb3246d3e7279f0de81c96
Oowazu Nonowazu <oowazu.nonowazu@gmail.com>
2022-10-24 13:30:40 -0500

    status effect basic docs

--- nari/types/event/status.py
+++ nari/types/event/status.py
@@ -1,4 +1,7 @@
-"""Classes for status events"""
+"""
+Status effects
+~~~~~~~~~~~~~~
+"""
 from nari.types import Timestamp
 from nari.types.event import Event
 from nari.types.actor import Actor
@@ -6,7 +9,21 @@ from nari.types.status import Status
 
 
 class StatusApply(Event): # pylint: disable=too-few-public-methods
-    """Represents the application of a status"""
+    """Represents the application of a status
+
+    :param timestamp: The timestamp of the event
+    :type timestamp: Timestamp
+    :param status: The status effect being applied
+    :type status: Status
+    :param duration: The duration the status will be in effect for
+    :type duration: float
+    :param source_actor: The actor that applied the effect
+    :type source_actor: Actor
+    :param target_actor: The actor that is the recipient of the effect
+    :type target_actor: Actor
+    :param params: TODO:
+    :type params: int
+    """
     def __init__(self, *,
                  timestamp: Timestamp,
                  status: Status,
@@ -27,7 +44,21 @@ class StatusApply(Event): # pylint: disable=too-few-public-methods
 
 
 class StatusRemove(Event): # pylint: disable=too-few-public-methods
-    """Represents the removal of a status"""
+    """Represents the removal of a status
+
+    :param timestamp: The timestamp of the event
+    :type timestamp: Timestamp
+    :param status: The status effect being removed
+    :type status: Status
+    :param duration: TODO: why is there a duration when it's removed??
+    :type duration: float
+    :param source_actor: TODO: why do we care about a source actor when it's gone?
+    :type source_actor: Actor
+    :param target_actor: The actor that the effect is being removed from
+    :type target_actor: Actor
+    :param params: TODO:
+    :type params: int
+    """
     def __init__(self, *,
                  timestamp: Timestamp,
                  status: Status,
--- nari/types/event/statuslist.py
+++ nari/types/event/statuslist.py
@@ -1,4 +1,7 @@
-"""Classes for status list updates"""
+"""
+Status Effect Lists
+~~~~~~~~~~~~~~~~~~~
+"""
 from nari.types import Timestamp
 from nari.types.event import Event
 from nari.types.classjoblevel import ClassJobLevel
@@ -7,7 +10,17 @@ from nari.types.status import StatusEffect
 
 
 class StatusList(Event): # pylint: disable=too-few-public-methods
-    """Represents a player Actor status list"""
+    """Represents a player Actor status list
+
+    :param timestamp: The timestamp of the event
+    :type timestamp: Timestamp
+    :param class_job_level: The actor's class, job, and level
+    :type class_job_level: ClassJobLevel
+    :param target_actor: The target actor that has status effects
+    :type target_actor: Actor
+    :param status_effects: A list of status effects
+    :type status_effects: list[StatusEffect]
+    """
     def __init__(self, *,
                  timestamp: Timestamp,
                  class_job_level: ClassJobLevel,
@@ -24,7 +37,15 @@ class StatusList(Event): # pylint: disable=too-few-public-methods
 
 
 class StatusListBasic(Event): # pylint: disable=too-few-public-methods
-    """Represents a simplified status list"""
+    """Represents a simplified status list as opposed to ``StatusList``
+
+    :param timestamp: The timestamp of the event
+    :type timestamp: Timestamp
+    :param target_actor: The target actor that has status effects
+    :type target_actor: Actor
+    :param status_effects: A list of status effects
+    :type status_effects: list[StatusEffect]
+    """
     def __init__(self, *,
                  timestamp: Timestamp,
                  target_actor: Actor,
